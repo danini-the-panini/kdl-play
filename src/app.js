689ad029985b3556c4b6736107ab906408a2ee16
Danielle Smith <danini@hey.com>
2023-06-29 19:54:07 +0200

    add basic kdl input/output

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hello KDL !');
+import { parse } from 'kdljs'
+
+addEventListener("DOMContentLoaded", (event) => {
+  const input = document.getElementById('input')
+  const output = document.getElementById('output')
+
+  function doParse() {
+    try {
+      let result = parse(input.value)
+
+      output.textContent = JSON.stringify(result, null, 2)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  input.addEventListener('input', doParse)
+
+  doParse()
+})
